6d0b1cfeccad9a2f3eae42770b3bc98de85d0dba
Rami2212 <ramithapathmilarp@gmail.com>
2026-03-22 23:16:38 +0530

    initial commit

---
+++ Models/Task.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace TimeTableApp.Models
+{
+    public class Task : INotifyPropertyChanged
+    {
+        private string _name = string.Empty;
+        private int _points;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        public int Points
+        {
+            get => _points;
+            set
+            {
+                if (_points != value)
+                {
+                    _points = value;
+                    OnPropertyChanged(nameof(Points));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTableApp.ViewModels
+{
+    internal class MainViewModel
+    {
+    }
+}
