6647441e44588e088f79048b9729b99f0c83d991
Rami2212 <ramithapathmilarp@gmail.com>
2026-03-23 14:49:37 +0530

    add task

--- Models/DayTaskStatus.cs
+++ Models/DayTaskStatus.cs
@@ -1,10 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
 
 namespace TimeTableApp.Models
 {
-    internal class DayTaskStatus
+    public class DayTaskStatus : INotifyPropertyChanged
     {
+        private bool _isDone;
+
+        public Task Task { get; }
+
+        public string TaskName => Task.Name;
+        public int Points => Task.Points;
+
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                if (_isDone != value)
+                {
+                    _isDone = value;
+                    OnPropertyChanged(nameof(IsDone));
+                }
+            }
+        }
+
+        public DayTaskStatus(Task task)
+        {
+            Task = task;
+
+            // When task name or points change, notify the UI
+            Task.PropertyChanged += (_, __) =>
+            {
+                OnPropertyChanged(nameof(TaskName));
+                OnPropertyChanged(nameof(Points));
+            };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
