fd41746e557d6742efa535b728a083c85acda31f
Rami2212 <ramithapathmilarp@gmail.com>
2026-07-03 01:12:45 +0530

    header

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows;
+using System.Reflection;
+using System.Windows;
 using TimeTableApp.ViewModels;
 
 namespace TimeTableApp
 {
     public partial class MainWindow : Window
     {
+        public string AppVersion =>
+            Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.0.0";
+
         public MainWindow()
         {
             InitializeComponent();
